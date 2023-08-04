2d93a7bdae6600771c669b1925f0218985e8230f
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-04 08:25:51 -0700

    Add readme.md and class Stack

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -1,17 +1,39 @@
 class Stack
     def initialize
-
+        @stack = []
     end
 
     def push(el)
-
+        @stack << el
     end
 
     def pop
+        @stack.shift
+    end
+
+    def peek
+        @stack[0]
+    end
+end
+
+class Queue 
+    def initialize
+
+    end
+
+    def enqueue(el)
+
+    end
+
+    def dequeue
 
     end
 
     def peek
 
     end
+end
+
+class Map
+
 end
