3dc2cc242f2fe85cfe4efbf0fa75320e30256cd2
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-04 08:31:16 -0700

    Add instances methods to Queue class

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -18,19 +18,19 @@ end
 
 class Queue 
     def initialize
-
+        @queue = []
     end
 
     def enqueue(el)
-
+        @queue << el
     end
 
     def dequeue
-
+        @queue.delete_at(-1)
     end
 
     def peek
-
+        @stack.last
     end
 end
 
