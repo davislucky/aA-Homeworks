a82978e7a1e4d7120060c41e838d5f3cb05ca6f7
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-09 08:14:40 -0700

    Finish Simon project

--- W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
+++ W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
@@ -4,29 +4,60 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
+    
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
+    self.add_random_color
   end
 
   def require_sequence
+    puts "Repeat the sequence, friend."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
   end
 
   def add_random_color
+    @seq << COLORS.sample
   end
 
   def round_success_message
+    puts "!GOOD joB!"
   end
 
   def game_over_message
+    puts "YOU LOSE!"
   end
 
   def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
